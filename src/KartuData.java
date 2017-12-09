a55995c8c6440711652919021fb5033ac7509826
goeltomeijer <meijergoeltom@gmail.com>
2017-12-09 22:56:01 +0700

    kartuData

---
+++ src/KartuData.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author weare
+ */
+public class KartuData {
+    String provider;
+    String nama;
+    String warna;
+    String bentuk;
+    int harga;
+    int kuota;
+    int masaAktif;
+    Boolean baru;
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public String getBentuk() {
+        return bentuk;
+    }
+
+    public void setBentuk(String bentuk) {
+        this.bentuk = bentuk;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    public int getKuota() {
+        return kuota;
+    }
+
+    public void setKuota(int kuota) {
+        this.kuota = kuota;
+    }
+
+    public int getMasaAktif() {
+        return masaAktif;
+    }
+
+    public void setMasaAktif(int masaAktif) {
+        this.masaAktif = masaAktif;
+    }
+
+    public Boolean getBaru() {
+        return baru;
+    }
+
+    public void setBaru(Boolean baru) {
+        this.baru = baru;
+    }
+    
+    
+}
